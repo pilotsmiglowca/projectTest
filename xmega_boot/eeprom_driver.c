81101214f13efafd3c0bd0c7bb94e177bd4962ea
grzesiomaster <grzesiomaster@o2.pl>
2017-06-18 22:26:08 +0200

    branch A

--- xmega_boot/eeprom_driver.c
+++ xmega_boot/eeprom_driver.c
@@ -55,7 +55,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *****************************************************************************/
-
+//branch A
 #include "eeprom_driver.h"
 
 /*! \brief Write one byte to EEPROM using IO mapping.
